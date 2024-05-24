e107fc4f5efd1b8546fe8ef07b0c3a0d5e11ed9a
sdilolle <stephdilolle@gmail.com>
2024-05-23 22:19:07 -0400

    updated index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Products.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongToMany(Tags, {
+  foreignKey: 'ProductTag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongToMany(Products, {
+  foreignKey: 'ProductTag',
+});
 
 module.exports = {
   Product,
